b3fb5610ffeae83cfc1c56b7f1b99ea4c0cc134a
Tomasz Pietrzak <tomasz.pietrzak@pietrum.pl>
2015-09-03 16:49:34 +0200

    feat(test): Verified exposed data.

---
+++ test/expose.spec.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var expect = require('chai').expect;
+var bmq = require('../');
+
+describe('[Expose]', function() {
+  it('should expose as object', function() {
+    expect(bmq).to.be.a('object');
+  });
+
+  it('should expose a Connection enum', function() {
+    expect(bmq.Connection).to.be.a('object');
+  });
+
+  it('should expose a Heartbeating enum', function() {
+    expect(bmq.Heartbeating).to.be.a('object');
+  });
+
+  it('should expose a Pattern enum', function() {
+    expect(bmq.Pattern).to.be.a('object');
+  });
+
+  it('should expose a Client type', function() {
+    expect(bmq.Client).to.be.a('function');
+  });
+
+  it('should expose a Broker type', function() {
+    expect(bmq.Broker).to.be.a('function');
+  });
+
+  it('should expose a Worker type', function() {
+    expect(bmq.Worker).to.be.a('function');
+  });
+});
