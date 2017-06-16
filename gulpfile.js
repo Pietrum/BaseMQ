736462d266f46dbf79d7f9357606e9dd0542a494
Tomasz Pietrzak <tomasz.pietrzak@pietrum.pl>
2017-06-16 17:33:33 +0200

    contrib(project): Refactoring initial configuration.

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,53 @@
 'use strict';
 
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
-var path = require('path');
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
+const path = require('path');
 
-var PATH = {
+const PATH = {
   example: 'examples',
   library: 'lib',
-  test: 'test'
+  test: 'test',
 };
-var TESTS = [
+const TESTS = [
   // enum
-  path.join('.', PATH.test, 'enum.spec.js'),
+  // path.join('.', PATH.test, 'enum.spec.js'),
   // module
-  path.join('.', PATH.test, 'pubsub.spec.js'),
-  path.join('.', PATH.test, 'pushpull.spec.js'),
-  path.join('.', PATH.test, 'reqrep.spec.js'),
-  path.join('.', PATH.test, 'send.spec.js'),
+  // path.join('.', PATH.test, 'pubsub.spec.js'),
+  // path.join('.', PATH.test, 'pushpull.spec.js'),
+  // path.join('.', PATH.test, 'reqrep.spec.js'),
+  // path.join('.', PATH.test, 'send.spec.js'),
   // core
-  path.join('.', PATH.test, 'basemq.spec.js')
+  path.join('.', PATH.test, 'basemq.test.js'),
 ];
-var LIBRARY = [
-  path.join('.', 'index.js'),
+const LIBRARY = [
+  // path.join('.', 'index.js'),
   // enum
-  path.join('.', PATH.library, 'enum', 'module.js'),
-  path.join('.', PATH.library, 'enum', 'socket.js'),
+  // path.join('.', PATH.library, 'enum', 'module.js'),
+  // path.join('.', PATH.library, 'enum', 'socket.js'),
   // module
-  path.join('.', PATH.library, 'module', 'config.js'),
-  path.join('.', PATH.library, 'module', 'connection.js'),
-  path.join('.', PATH.library, 'module', 'message.js'),
-  path.join('.', PATH.library, 'module', 'socket.js'),
+  // path.join('.', PATH.library, 'module', 'config.js'),
+  // path.join('.', PATH.library, 'module', 'connection.js'),
+  // path.join('.', PATH.library, 'module', 'message.js'),
+  // path.join('.', PATH.library, 'module', 'socket.js'),
   // core
-  path.join('.', PATH.library, 'basemq.js')
+  path.join('.', PATH.library, 'basemq.js'),
 ];
-var FILES = [
-  path.join('.', 'gulpfile.js')
+const FILES = [
+  path.join('.', 'gulpfile.js'),
 ].concat(LIBRARY).concat(TESTS);
 
-gulp.task('jscs', function () {
-  return gulp.src(FILES)
-    .pipe(plugins.jscs())
-    .pipe(plugins.jscs.reporter())
-    .pipe(plugins.jscs.reporter('fail'));
-});
+gulp.task('lint', () => (gulp.src(FILES)
+  .pipe(plugins.eslint())
+  .pipe(plugins.eslint.format())
+  .pipe(plugins.eslint.failAfterError())
+));
 
-gulp.task('jshint', ['jscs'], function () {
-  return gulp.src(FILES)
-    .pipe(plugins.jshint())
-    .pipe(plugins.jshint.reporter('jshint-stylish'))
-    .pipe(plugins.jshint.reporter('fail'));
-});
-
-gulp.task('develop', ['jshint'], function () {
-  gulp.watch(FILES, ['jshint']);
+gulp.task('develop', () => {
+  gulp.watch(FILES, ['lint']);
   gulp.watch(TESTS, ['test']);
 });
 
-gulp.task('test', ['jshint'], function () {
-  return gulp.src(TESTS, { read: false })
-    .pipe(plugins.mocha({ reporter: 'spec' }));
-});
+gulp.task('test', ['lint'], () => (gulp.src(PATH.test, { read: false })
+  .pipe(plugins.jest.default())
+));
---
+++ test/basemq.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * Load test module.
+ */
+const BMQ = require('../lib/basemq');
+
+test('should be defined', () => {
+  expect(BMQ).toBeDefined();
+});
